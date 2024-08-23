8da58bc652b04a10dda790ab4cfb2669e4359745
Srihari Nair <sriharinair134@gmail.com>
2024-08-23 10:54:53 +0530

    10_3.cpp

--- 10_3.cpp
+++ 10_3.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
-using namespace std;
-void swap(int x, int y)
+//Name: Srihari Nair
+//Prn: 23070123131
+//Class: EnTC B-2
+#include<iostream> 
+using namespace std; 
+void swap(int *x, int *y) 
 {
-int swap;
-swap=x;
-x=y;
-y=swap;
-cout<<"Value of a is: "<<x<<endl;
-cout<<"Value of b is: "<<y<<endl;
+    int swap;
+    swap=*x;
+    *x=*y;
+    *y=swap;
 }
-
-int main()
+int main() 
 {
-int a=5, b=2;
-swap(a,b);
-return 0;
+    int a=4,b=7;
+    swap(&a,&b);
+    cout<<"Value of a is: "<<a<<"\n";
+    cout<<"Value of b is: "<<b<<"\n";
+    return 0;
 }
